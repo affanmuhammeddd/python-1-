5ffe0971a56a52f2b193b482fe97573b8bb55522
affanmuhammeddd <affansjcet@gmail.com>
2024-10-04 16:07:47 +0530

    Add files via upload

---
+++ the pythooonnnnnnn.py
@@ -0,0 +1,52 @@
+'''
+print("hello\nworld")
+
+if (44>40):
+    print("kollam poli sanammmm")
+if (44>40):
+ print("kollam poli sanammmm")
+
+x = 5
+y = "Thomachan AYYO AYYO AYEYYEYOOOOOOOOOOOOOOOOO"
+#print (x)
+print (y)
+
+x =str(3) # x will be '3'
+y =int(3) # y will be 3
+z =float(3) # z will be 3.0
+print (x)
+print (y)
+print (z)
+
+x = 5
+y = 4.444
+z = "ammmmmmmmeeeeeeeeeee"
+print (type (x))
+print (type (y))
+print (type (z))
+x, y, z = "orange" , "banana", "cherry"
+print(x)
+print(y)
+print(z)
+x = y = z = "orange kilo 50e
+x = "python "
+a = "hello "eeeeeeeeeeeeeeeeeeeeeee"
+y = "is "
+username = input("enter usern
+z = "awesome"
+print(x + y + z)ame:")
+print("username is:" + username)
+print(x)
+print(y)
+print(z)
+b = "appukutta"
+c = a + b
+print(c)
+'''
+
+
+
+
+
+
+
